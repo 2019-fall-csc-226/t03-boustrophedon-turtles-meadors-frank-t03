109d9d63bf01753348a1108086ace859fb686190
frankb <frankb@berea.edu>
2019-09-10 03:04:23 -0400

    Started Square and Boolean Added Repo Link, Google Doc, and Names

--- t03_stub.py
+++ t03_stub.py
@@ -18,57 +18,72 @@ wn.bgcolor('black')
 
 meadors = turtle.Turtle()
 meadors.color('deep sky blue')
-meadors.pensize(20)
+meadors.pensize(25)
 #meadors.penup()
 
-def square_turtle(name, x, y, size):
+def square_turtle(x, y, size):
     """
     Docstring for function_1
     """
 
-    name.penup()
-    name.goto(x, y)
-    name.pendown()
-    name.setheading(0)
+    meadors.penup()
+    meadors.goto(x, y)
+    meadors.pendown()
+    meadors.setheading(0)
     for side in range(4):
-        name.forward(size)
-        name.left(90)
+        meadors.forward(size)
+        meadors.left(90)
 
     # ...
 
 
-def squiggle_right(name, x, y):
+def squiggle_right():
     """
     Docstring for function_2
     """
-    name.goto(x, y)
-    name.setheading(90)
-    name.pencolor('green')
-    for yeeee in range(12):
-        name.forward(20)
-        name.right(90)
-        name.forward(20)
-        name.right(90)
-        name.forward(20)
-        name.left(90)
-        name.forward(20)
-        name.left(90)
+    for cross_space in range(12):
+        meadors.forward(20)
+        meadors.right(90)
+        meadors.forward(20)
+        meadors.right(90)
+        meadors.forward(20)
+        meadors.left(90)
+        meadors.forward(20)
+        meadors.left(90)
     # ...
 
 def squiggle_left():
     """
     Docstring for function_2
     """
-    pass
+    for cross_space in range(12):
+        meadors.left(90)
+        meadors.forward(20)
+        meadors.left(90)
+        meadors.forward(20)
+        meadors.right(90)
+        meadors.forward(20)
+        meadors.right(90)
+        meadors.forward(20)
     # ...
 
 def main():
     """
     Docstring for main
     """
+    meadors.speed(0)
+    square_turtle(-260, -260, 500)
+    meadors.penup()
+    meadors.pencolor('green')
+    meadors.goto(-240, -240)
+    meadors.pendown()
+    meadors.setheading(90)
+    for fill in range(4):
+        squiggle_right()
+        meadors.forward(100)
+        squiggle_left()
+        meadors.forward(20)
     # ...
 
-
-square_turtle(meadors, -260, -260, 520)
-squiggle_right(meadors, -240, -240)
+main()
 wn.exitonclick()
